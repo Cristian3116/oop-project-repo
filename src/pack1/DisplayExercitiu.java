57364052d33290ec857ced10ec7ce21c842c7c46
Mija Cristian <mija.cristian3101@gmail.com>
2025-10-04 13:44:20 +0300

    Add package pack1

---
+++ src/pack1/DisplayExercitiu.java
@@ -0,0 +1,13 @@
+package pack1;
+
+import java.sql.SQLOutput;
+
+public class DisplayExercitiu {
+    public static void main(String[] args) {
+
+        Exercitiu afisare = new Exercitiu();
+        System.out.println(afisare.nr1);
+        System.out.println(afisare.nr2);
+    }
+
+}
