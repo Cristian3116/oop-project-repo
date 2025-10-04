f5f97f63968247e3c727d2ed9376ccc315c7d047
Mija Cristian <mija.cristian3101@gmail.com>
2025-10-04 14:13:51 +0300

    Add package pack3

---
+++ src/pack3/Hobby.java
@@ -0,0 +1,9 @@
+package pack3;
+
+public class Hobby {
+
+    public String nume    = "Terentev";
+    public String prenume = "Liviu";
+    public int age        = 18;
+    public String hobby   = "Dansator";
+}
---
+++ src/pack3/HobbyDisplay.java
@@ -0,0 +1,12 @@
+package pack3;
+
+public class HobbyDisplay {
+    public static void main(String[] args) {
+
+        Hobby disp = new Hobby();
+        System.out.println(disp.nume);
+        System.out.println(disp.prenume);
+        System.out.println(disp.age);
+        System.out.println(disp.hobby);
+    }
+}
